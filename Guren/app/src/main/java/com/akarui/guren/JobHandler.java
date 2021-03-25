2d3a549797f24b4515ec900f703e4a3471295268
duongluu <duongluu.bh2k@gmail.com>
2021-03-25 20:57:40 +0700

    + jobhandle

--- Guren/app/src/main/java/com/akarui/guren/JobHandler.java
+++ Guren/app/src/main/java/com/akarui/guren/JobHandler.java
@@ -1,47 +1,62 @@
 package com.akarui.guren;
 
 import android.app.AlarmManager;
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
-import androidx.core.app.JobIntentService;
-import androidx.core.app.NotificationCompat;
-
 import com.akarui.guren.database.Converter;
 import com.akarui.guren.database.entity.Job;
+import com.akarui.guren.service.JobService;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
-import lombok.Getter;
-import lombok.Setter;
+import java.time.temporal.ChronoUnit;
 
 public class JobHandler {
-//    /**
-//     * @param job
-//     * @param context
-//     * @param duration duration before deadline
-//     */
-//    public static void createJobNotification(Job job, Context context, Duration duration){
-//        if (job.getDeadline().compareTo(LocalDateTime.now()) <= 0){
-//            return;
-//        }
-//
-//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-//        Intent intent = new Intent(context, JobService.class);
-//        intent.putExtra("JobId", job.getId());
-//
-//        PendingIntent deadlineIntent = PendingIntent.getActivity(context, job.getId(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
-//        alarmManager.setExact(AlarmManager.RTC_WAKEUP, Converter.ToTimeStamp(job.getDeadline().minus(duration)), deadlineIntent);
-//    }
-//
-//    public static void deleteJobNotification(Job job, Context context){
-//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-//        Intent intent = new Intent(context, JobService.class);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(context, job.getId(), intent, PendingIntent.FLAG_NO_CREATE);
-//        alarmManager.cancel(pendingIntent);
-//    }
+    public static final long[] TIMES_BEFORE_DEADLINE_NOTIFICATION = {48 * 60, 24 * 60, 60, 30, 15};
+    
+    public static void createJobNotification(Job job, Context context){
+        LocalDateTime deadline = job.getDeadline();
+        LocalDateTime now = LocalDateTime.now();
+        
+        long minutes = now.until(deadline, ChronoUnit.MINUTES);
+        
+        int i;
+        for (i = 0; i < TIMES_BEFORE_DEADLINE_NOTIFICATION.length; i++){
+            if (minutes < TIMES_BEFORE_DEADLINE_NOTIFICATION[i]){
+                break;
+            }
+        }
+    
+        if (i >= TIMES_BEFORE_DEADLINE_NOTIFICATION.length){
+            return;
+        }
+        createJobNotification(job, context, Duration.ofHours(TIMES_BEFORE_DEADLINE_NOTIFICATION[i]));
+    }
+    
+    /**
+     * @param job
+     * @param context
+     * @param duration duration before deadline
+     */
+    public static void createJobNotification(Job job, Context context, Duration duration){
+        if (job.getDeadline().compareTo(LocalDateTime.now()) <= 0){
+            return;
+        }
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, JobService.class);
+        intent.putExtra("JobId", job.getId());
+
+        PendingIntent deadlineIntent = PendingIntent.getActivity(context, job.getId(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, Converter.ToTimeStamp(job.getDeadline().minus(duration)), deadlineIntent);
+    }
+
+    public static void deleteJobNotification(Job job, Context context){
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, JobService.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, job.getId(), intent, PendingIntent.FLAG_NO_CREATE);
+        alarmManager.cancel(pendingIntent);
+    }
 }
