91e60ce964885c3737f84ccf75b151869586d310
hson-fpt <sonhh@topcv.vn>
2021-03-31 21:44:29 +0700

    add onClick Time Listener

--- Guren/app/src/main/java/com/akarui/guren/UI/Task/AddTaskActivity.java
+++ Guren/app/src/main/java/com/akarui/guren/UI/Task/AddTaskActivity.java
@@ -3,10 +3,12 @@ package com.akarui.guren.UI.Task;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.akarui.guren.R;
 import com.akarui.guren.database.GurenDatabase;
@@ -14,6 +16,8 @@ import com.akarui.guren.database.entity.Job;
 
 import java.text.SimpleDateFormat;
 import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -33,23 +37,25 @@ public class AddTaskActivity extends AppCompatActivity {
     }
 
     private void renderDatePicker() {
-        int fromId = R.id.task_from_date_value;
-        int toId = R.id.task_to_date_value;
+        int fromDateValue = R.id.task_from_date_value;
+        int toDateValue = R.id.task_to_date_value;
+        int fromTimeValue = R.id.task_from_time_value;
+        int toTimeValue = R.id.task_to_time_value;
 
-        addChangeDateListener(fromId);
-        addChangeDateListener(toId);
+        addChangeDateListener(fromDateValue, fromTimeValue);
+        addChangeDateListener(toDateValue, toTimeValue);
     }
 
     private void updateLabel(EditText editText, Calendar calendar) {
         String dateFormat = "yyyy/MM/dd";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.US);
         editText.setText(simpleDateFormat.format(calendar.getTime()));
-
     }
 
-    private void addChangeDateListener(int editTextId) {
+    private void addChangeDateListener(int dateTextId, int timeTextId) {
         final Calendar calendar = Calendar.getInstance();
-        EditText editText = (EditText) findViewById(editTextId);
+        EditText dateText = (EditText) findViewById(dateTextId);
+        EditText timeText = (EditText) findViewById(timeTextId);
 
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -57,14 +63,46 @@ public class AddTaskActivity extends AppCompatActivity {
                 calendar.set(Calendar.YEAR, year);
                 calendar.set(Calendar.MONTH, month);
                 calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel(editText, calendar);
+                updateLabel(dateText, calendar);
             }
         };
 
-        editText.setOnClickListener(new View.OnClickListener() {
+        TimePickerDialog.OnTimeSetListener time = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                timeText.setText(hourOfDay + " : " + minute );
+            }
+        };
+
+        dateText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new DatePickerDialog(AddTaskActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        timeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mHour = c.get(Calendar.HOUR_OF_DAY);
+                int mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(AddTaskActivity.this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            timeText.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+                timePickerDialog.show();
+                
             }
         });
     }
